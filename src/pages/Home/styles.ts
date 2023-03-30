0793d71f8920c18d4e4c70b40f1d7497d5ade4b7
Marcinho Coutinho <marciojscoutinho@gmail.com>
2023-03-30 13:58:29 -0300

    corrgindo erro

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,7 +7,24 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
 `
 
-export const FormContainer = styled.div``
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
 export const CountdownContainer = styled.div``
